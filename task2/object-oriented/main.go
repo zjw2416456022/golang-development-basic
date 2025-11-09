436e11c6167093d2df825a8c787cde88e46a3cdd
zjw <2416456022@qq.com>
2025-11-09 23:32:16 +0800

    任务二：面向对象-1-未完成

---
+++ task2/object-oriented/main.go
@@ -0,0 +1,22 @@
+/*
+	[面向对象]
+	题目 ：定义一个 Shape 接口，包含 Area() 和 Perimeter() 两个方法。然后创建 Rectangle 和 Circle 结构体，实现 Shape 接口。在主函数中，创建这两个结构体的实例，并调用它们的 Area() 和 Perimeter() 方法。
+		考察点 ：接口的定义与实现、面向对象编程风格。
+	题目 ：使用组合的方式创建一个 Person 结构体，包含 Name 和 Age 字段，再创建一个 Employee 结构体，组合 Person 结构体并添加 EmployeeID 字段。为 Employee 结构体实现一个 PrintInfo() 方法，输出员工的信息。
+		考察点 ：组合的使用、方法接收者。
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	p := 1
+	fmt.Println(p)
+}
+
+// type it
+
+func Area() {
+
+}
